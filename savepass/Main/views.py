749978f52563f85d515b867a41ac5fe639fb9a98
unknown <pqrlink@gmail.com>
2024-11-07 17:39:25 +0530

    added form post method

--- savepass/Main/views.py
+++ savepass/Main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+variable = {}
 # Create your views here.
 def index(request):
     return HttpResponse("Hi this is me")
 def login(request):
-    return render(request , "login.html")
+    if request.method == "POST":
+        variable= request.POST
+        return render(request , "login.html", variable)
+    else:        
+        return render(request , "login.html")
 def signup(request):
     return render(request , "signup.html")
