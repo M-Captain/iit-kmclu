aa83e6c68bec41055eee6af206b7e18487405f07
unknown <pqrlink@gmail.com>
2024-11-08 00:30:22 +0530

    added vercel config

--- savepass/SavePass/settings.py
+++ savepass/SavePass/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d=&^b%98cxldre@9bfv0a=+hbh#ew@d=u(80-tr89nh6-x-t)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
